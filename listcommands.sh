b7aa0ff72e09629539898512f12d27ab0e2e92e2
ag143 <ag14341@gmail.com>
2021-09-09 05:52:18 +0530

    Create listcommands.sh

---
+++ listcommands.sh
@@ -0,0 +1,148 @@
+Essential user command binaries(/bin)
+----------------------------------------
+cat to display commands
+chmod to change  read write execute permissions
+cp to copy content from one folder to another
+date to display current or mentioned date
+echo  to display the message we pass
+grep
+gunzip
+gzip
+hostname
+kill
+less
+ln
+ls
+mkdir
+more
+mount
+mv
+nano
+open
+ping
+ps
+pwd
+rm
+sh
+su
+tar
+touch
+umount
+uname
+
+/configuration files for the system
+--------------------------------------
+crontab
+cups
+fonts
+fstab
+host.conf
+hostname
+hosts
+hosts.allow
+hosts.deny
+init
+init.d
+issue
+machine-id
+mtab
+mtools.conf
+nanorc
+networks
+passwd
+profile
+protocols
+resolv.conf
+rpc
+securetty
+servics
+shells
+timezone
+
+Essential System binaries
+-----------------------------
+fdisk
+fsck
+getty
+halt
+ifconfig
+init
+mkfs
+mkswap
+reboot
+route
+
+read-only user application support data & binaries
+--------------------------------------------------
+/usr/bin  most user commands
+/usr/include standard include files for c code
+/user/lib/  obj, bin, lib files for coding and packages
+/usr/local local software  /usr/local/bin /usr/local/lib /usr/local/man /usr/local/man /usr/local/sbin /usr/local/share
+
+static data shareable across all architectures
+-----------------------------------------------
+/usr/share/man
+
+variable data files
+------------------
+/var
+
+application cache data
+-----------------------
+/var/cache
+
+data modified as programmes run
+------------------------------------
+/var/lib/
+
+lock files to track resources in use
+--------------------------------------
+/var/lock
+
+log files
+--------------
+/var/log
+
+variable data for installed packages
+--------------------------------------
+/var/opt
+
+tasks waiting to be processed
+----------------------------------
+/var/spool
+/var/spool/cron
+/var/spool/cups
+/var/spool/mail
+
+temporary files saved between reboots
+-------------------------------------
+/var/tmp
+
+device files includ /dev/null
+-------------------------------
+/dev
+
+user home directories
+--------------------------
+/home
+
+librarires and kernel modules
+------------------------------
+/lib
+
+mount files for temporary filesystems
+---------------------------------------
+/mnt
+
+optional software applications
+-----------------------------------
+/opt
+
+process & kernel information files
+------------------------------------
+/proc
+
+home dir for root users
+-------------------------
+/root
+
