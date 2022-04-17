f26f923d20d760ec0d4cfaa4c35a3fadabe77d0f
ag143 <ag14341@gmail.com>
2022-04-17 21:53:17 +0530

    Update

--- basicCommands.sh
+++ basicCommands.sh
@@ -1,3 +1,20 @@
 echo "this is echo coming from shell script"
 uname
 cat listcommands.sh
+uname -s
+uname -r
+uname -v
+uname -n
+uname -m
+uname -p
+uname -i
+uname -o
+uname -a
+lshw
+lshw -short
+lshw -html > hardwareinfo.html
+lscpu
+lsblk -a
+lsusb
+lsusb -a
+
