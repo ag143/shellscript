82ac80fa7d3af1b5aee425ccb71d32cb9fc1cf12
ag143 <ag14341@gmail.com>
2022-04-17 22:04:43 +0530

    Update basicCommands.sh

--- basicCommands.sh
+++ basicCommands.sh
@@ -1,20 +1,42 @@
 echo "this is echo coming from shell script"
 uname
 cat listcommands.sh
-uname -s
-uname -r
-uname -v
-uname -n
-uname -m
-uname -p
-uname -i
-uname -o
-uname -a
-lshw
-lshw -short
-lshw -html > hardwareinfo.html
-lscpu
-lsblk -a
-lsusb
-lsusb -a
 
+
+echo "this is echo coming from shell script"
+echo "uname "
+uname
+echo "cat listcommands.sh"
+cat listcommands.sh
+echo " uname -s"
+uname -s
+echo " uname -r"
+uname -r
+echo " uname -v"
+uname -v
+echo " uname -n"
+uname -n
+echo " uname -m"
+uname -m
+echo " uname -p"
+uname -p
+echo " uname -i"
+uname -i
+echo " uname -o"
+uname -o
+echo " uname -r"
+uname -a
+echo " lshw"
+lshw
+echo " lshw -short"
+lshw -short
+echo " uname -r"
+lshw -html > hardwareinfo.html
+echo " lscpu"
+lscpu
+echo " lsblk -a"
+lsblk -a
+echo " lsusb"
+lsusb
+echo " lsusb -a"
+lsusb -a
