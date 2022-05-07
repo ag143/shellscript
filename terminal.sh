cc54626869bbcbc432e193b8ce47e2c96e396908
ag143 <ag14341@gmail.com>
2022-05-08 01:18:48 +0530

    Update terminal.sh

--- terminal.sh
+++ terminal.sh
@@ -1,1 +1,9 @@
+#learn bash scripting
+#VIM/NANO/Poershell/Emacs
+#Compiling apps from source(gcc,make)
+#SYstem performance(nmon,iostat,sar,vmstat)
+#others(strace,dtrace,systemtap,uname,df,history)
+#network(nmap,tcpdump,ping,mtr,traceroute,digariman,airodump,dig,iptables,netstat)
+#process Monitoring(ps,top,htop,atop,lsof)
+#Text Manipulation TOols(awk,sed,grep,sort,uniq,cat,cut,echo,frmt,tr,nl,egrep,fgrep,wc)
 
