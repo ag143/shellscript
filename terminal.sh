1716a5f69b381dd1d37089e89cd83b26d6bfa017
ag143 <ag14341@gmail.com>
2022-05-08 01:29:31 +0530

    Update terminal.sh

--- terminal.sh
+++ terminal.sh
@@ -1,11 +1,19 @@
 #learn bash scripting
+echo "# learn Bash scripting" >shell_quick_guide.md
 #VIM/NANO/Poershell/Emacs
+echo "# VIM/NANO/Poershell/Emacs" >shell_quick_guide.md
 #Compiling apps from source(gcc,make)
+echo "# Compiling apps from source(gcc,make)" >shell_quick_guide.md
 #SYstem performance(nmon,iostat,sar,vmstat)
+echo "# System performance(nmon,iostat,sar,vmstat)" >shell_quick_guide.md
 #others(strace,dtrace,systemtap,uname,df,history)
+echo "# others(strace,dtrace,systemtap,uname,df,history)" >shell_quick_guide.md
 #network(nmap,tcpdump,ping,mtr,traceroute,digariman,airodump,dig,iptables,netstat)
+echo "# network(nmap,tcpdump,ping,mtr,traceroute,digariman,airodump,dig,iptables,netstat)" >shell_quick_guide.md
 #process Monitoring(ps,top,htop,atop,lsof)
+echo "# process Monitoring(ps,top,htop,atop,lsof)" >shell_quick_guide.md
 #Text Manipulation TOols(awk,sed,grep,sort,uniq,cat,cut,echo,frmt,tr,nl,egrep,fgrep,wc)
+echo "# Text Manipulation TOols(awk,sed,grep,sort,uniq,cat,cut,echo,frmt,tr,nl,egrep,fgrep,wc)" >shell_quick_guide.md
 git config --global user.email "ag14341@gmail.com"
 git config --global user.name "ag143"
 echo "working" >shell_detail_guide.md
