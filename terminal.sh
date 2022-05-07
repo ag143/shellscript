46c17ad7f5ea8d27c77a90021617ccd29324d6b8
ag143 <ag14341@gmail.com>
2022-05-08 01:36:56 +0530

    Update terminal.sh

--- terminal.sh
+++ terminal.sh
@@ -1,19 +1,33 @@
-#learn bash scripting
-echo "# learn Bash scripting" >shell_quick_guide.md
-#VIM/NANO/Poershell/Emacs
-echo "# VIM/NANO/Poershell/Emacs" >>shell_quick_guide.md
-#Compiling apps from source(gcc,make)
-echo "# Compiling apps from source(gcc,make)" >>shell_quick_guide.md
-#SYstem performance(nmon,iostat,sar,vmstat)
-echo "# System performance(nmon,iostat,sar,vmstat)" >>shell_quick_guide.md
-#others(strace,dtrace,systemtap,uname,df,history)
-echo "# others(strace,dtrace,systemtap,uname,df,history)" >>shell_quick_guide.md
-#network(nmap,tcpdump,ping,mtr,traceroute,digariman,airodump,dig,iptables,netstat)
-echo "# network(nmap,tcpdump,ping,mtr,traceroute,digariman,airodump,dig,iptables,netstat)" >>shell_quick_guide.md
-#process Monitoring(ps,top,htop,atop,lsof)
-echo "# process Monitoring(ps,top,htop,atop,lsof)" >>shell_quick_guide.md
-#Text Manipulation TOols(awk,sed,grep,sort,uniq,cat,cut,echo,frmt,tr,nl,egrep,fgrep,wc)
-echo "# Text Manipulation TOols(awk,sed,grep,sort,uniq,cat,cut,echo,frmt,tr,nl,egrep,fgrep,wc)" >>shell_quick_guide.md
+##learn bash scripting
+echo "## learn Bash scripting" >shell_quick_guide.md
+##VIM/NANO/Poershell/Emacs
+echo "## VIM/NANO/Poershell/Emacs" >>shell_quick_guide.md
+##Compiling apps from source(gcc,make)
+echo "## Compiling apps from source(gcc,make)" >>shell_quick_guide.md
+##SYstem performance(nmon,iostat,sar,vmstat)
+echo "## System performance(nmon,iostat,sar,vmstat)" >>shell_quick_guide.md
+##others(strace,dtrace,systemtap,uname,df,history)
+echo "## others(strace,dtrace,systemtap,uname,df,history)" >>shell_quick_guide.md
+##network(nmap,tcpdump,ping,mtr,traceroute,digariman,airodump,dig,iptables,netstat)
+echo "## network(nmap,tcpdump,ping,mtr,traceroute,digariman,airodump,dig,iptables,netstat)" >>shell_quick_guide.md
+##process Monitoring(ps,top,htop,atop,lsof)
+echo "## process Monitoring(ps,top,htop,atop,lsof)" >>shell_quick_guide.md
+##Text Manipulation TOols(awk,sed,grep,sort,uniq,cat,cut,echo,frmt,tr,nl,egrep,fgrep,wc)
+echo "## Text Manipulation TOols(awk,sed,grep,sort,uniq,cat,cut,echo,frmt,tr,nl,egrep,fgrep,wc)" >>shell_quick_guide.md
+echo "- awk" >>shell_quick_guide.md
+echo "- sed" >>shell_quick_guide.md
+echo "- grep" >>shell_quick_guide.md
+echo "- sort" >>shell_quick_guide.md
+echo "- uniq" >>shell_quick_guide.md
+echo "- cat" >>shell_quick_guide.md
+echo "- cut" >>shell_quick_guide.md
+echo "- echo" >>shell_quick_guide.md
+echo "- frmt" >>shell_quick_guide.md
+echo "- tr" >>shell_quick_guide.md
+echo "- nl" >>shell_quick_guide.md
+echo "- egrep" >>shell_quick_guide.md
+echo "- fgrep" >>shell_quick_guide.md
+echo "- wc" >>shell_quick_guide.md
 git config --global user.email "ag14341@gmail.com"
 git config --global user.name "ag143"
 echo "working" >>shell_detail_guide.md
