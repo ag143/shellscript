8c601c82e834cf8f7d0c800070257bc55143a442
ag143 <ag14341@gmail.com>
2022-05-08 13:35:01 +0530

    Update terminal.sh

--- terminal.sh
+++ terminal.sh
@@ -6,28 +6,94 @@ echo "## VIM/NANO/Poershell/Emacs" >>shell_quick_guide.md
 echo "## Compiling apps from source(gcc,make)" >>shell_quick_guide.md
 ##SYstem performance(nmon,iostat,sar,vmstat)
 echo "## System performance(nmon,iostat,sar,vmstat)" >>shell_quick_guide.md
+echo "- nmon" >>shell_quick_guide.md
+nmon >>shell_quick_guide.md
+echo "- iostat" >>shell_quick_guide.md
+iostat >>shell_quick_guide.md
+echo "- sar" >>shell_quick_guide.md
+sar >>shell_quick_guide.md
+echo "- vmstat" >>shell_quick_guide.md
+vmstat >>shell_quick_guide.md
 ##others(strace,dtrace,systemtap,uname,df,history)
 echo "## others(strace,dtrace,systemtap,uname,df,history)" >>shell_quick_guide.md
+echo "- strace" >>shell_quick_guide.md
+strace >>shell_quick_guide.md
+echo "- dtrace" >>shell_quick_guide.md
+dtrace >>shell_quick_guide.md
+echo "- systemtap" >>shell_quick_guide.md
+systemtap >>shell_quick_guide.md
+echo "- uname" >>shell_quick_guide.md
+uname >>shell_quick_guide.md
+echo "- df" >>shell_quick_guide.md
+df >>shell_quick_guide.md
+echo "- history" >>shell_quick_guide.md
+history >>shell_quick_guide.md
+
 ##network(nmap,tcpdump,ping,mtr,traceroute,digariman,airodump,dig,iptables,netstat)
 echo "## network(nmap,tcpdump,ping,mtr,traceroute,digariman,airodump,dig,iptables,netstat)" >>shell_quick_guide.md
+echo "- nmap" >>shell_quick_guide.md\
+nmap >>shell_quick_guide.md
+echo "- tcpdump" >>shell_quick_guide.md
+tcpdump >>shell_quick_guide.md
+echo "- ping" >>shell_quick_guide.md
+ping >>shell_quick_guide.md
+echo "- mtr" >>shell_quick_guide.md
+mtr >>shell_quick_guide.md
+echo "- traceroute" >>shell_quick_guide.md
+traceroute >>shell_quick_guide.md
+echo "- digariman" >>shell_quick_guide.md
+digariman >>shell_quick_guide.md
+echo "- airodump" >>shell_quick_guide.md
+airodump >>shell_quick_guide.md
+echo "- dig" >>shell_quick_guide.md
+dig >>shell_quick_guide.md
+echo "- iptables" >>shell_quick_guide.md
+iptables >>shell_quick_guide.md
+echo "- netstat" >>shell_quick_guide.md
+netstat >>shell_quick_guide.md
+
 ##process Monitoring(ps,top,htop,atop,lsof)
 echo "## process Monitoring(ps,top,htop,atop,lsof)" >>shell_quick_guide.md
+echo "- ps" >>shell_quick_guide.md
+ps >>shell_quick_guide.md
+echo "- top" >>shell_quick_guide.md
+top >>shell_quick_guide.md
+echo "- htop" >>shell_quick_guide.md
+htop >>shell_quick_guide.md
+echo "- atop" >>shell_quick_guide.md
+atop >>shell_quick_guide.md
+echo "- lsof" >>shell_quick_guide.md
+lsof >>shell_quick_guide.md
 ##Text Manipulation TOols(awk,sed,grep,sort,uniq,cat,cut,echo,frmt,tr,nl,egrep,fgrep,wc)
 echo "## Text Manipulation TOols(awk,sed,grep,sort,uniq,cat,cut,echo,frmt,tr,nl,egrep,fgrep,wc)" >>shell_quick_guide.md
 echo "- awk" >>shell_quick_guide.md
+awk >>shell_quick_guide.md
 echo "- sed" >>shell_quick_guide.md
+sed >>shell_quick_guide.md
 echo "- grep" >>shell_quick_guide.md
+grep >>shell_quick_guide.md
 echo "- sort" >>shell_quick_guide.md
+sort >>shell_quick_guide.md
 echo "- uniq" >>shell_quick_guide.md
+uniq >>shell_quick_guide.md
 echo "- cat" >>shell_quick_guide.md
+cat >>shell_quick_guide.md
 echo "- cut" >>shell_quick_guide.md
+cut >>shell_quick_guide.md
 echo "- echo" >>shell_quick_guide.md
+echo >>shell_quick_guide.md
 echo "- frmt" >>shell_quick_guide.md
+frmt >>shell_quick_guide.md
 echo "- tr" >>shell_quick_guide.md
+tr >>shell_quick_guide.md
 echo "- nl" >>shell_quick_guide.md
+nl >>shell_quick_guide.md
 echo "- egrep" >>shell_quick_guide.md
+egrep >>shell_quick_guide.md
 echo "- fgrep" >>shell_quick_guide.md
+fgrep >>shell_quick_guide.md
 echo "- wc" >>shell_quick_guide.md
+wc >>shell_quick_guide.md
 git config --global user.email "ag14341@gmail.com"
 git config --global user.name "ag143"
 echo "working" >>shell_detail_guide.md
